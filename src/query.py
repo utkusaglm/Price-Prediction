b156e20ef3afd3968d5441cce665f706c7ae5d4f
utku saglam <saglmutku@gmail.com>
2022-06-07 12:12:46 +0300

    streamlit

--- src/query.py
+++ src/query.py
@@ -2,12 +2,22 @@ import psycopg2
 from sqlalchemy import create_engine,text
 import pandas as pd
 import config
+import streamlit as st
 
-HOST = config.DB_HOST
-DATABASE = config.DB_NAME
-USER = config.DB_USER
-PASSWORD = config.DB_PASS
-PORT = config.DB_PORT
+# HOST = config.DB_HOST
+# DATABASE = config.DB_NAME
+# USER = config.DB_USER
+# PASSWORD = config.DB_PASS
+# PORT = config.DB_PORT
+
+HOST = st.secrets["postgres"]['host']
+DATABASE = st.secrets["postgres"]['dbname']
+USER = st.secrets["postgres"]['user']
+PASSWORD = st.secrets["postgres"]['password']
+PORT = st.secrets["postgres"]['port']
+
+
+st.secrets["postgres"]['host']
 
 QUERY = """select * from assets ORDER BY date ASC;"""
 CREATE_TABLE = """
@@ -32,8 +42,12 @@ class GetDataFromDatabase:
 
     def __init__(self,query) -> None:
         self.query = query
+        __import__('ipdb').set_trace()
         self.engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
+        # self.engine = create_engine(**st.secrets["postgres"])
         self.connection = psycopg2.connect(f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
+        # self.connection = psycopg2.connect(**st.secrets["postgres"])
+  
 
 get_data = GetDataFromDatabase(query=QUERY)
 try:
