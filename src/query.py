cb69c8acf2f9d07c8a82a093c6f62f9dc2f3d310
utku saglam <saglmutku@gmail.com>
2022-06-07 13:58:15 +0300

    refactoring

--- src/query.py
+++ src/query.py
@@ -2,20 +2,12 @@ import psycopg2
 from sqlalchemy import create_engine,text
 import pandas as pd
 import config
-import streamlit as st
 
 HOST = config.DB_HOST
 DATABASE = config.DB_NAME
 USER = config.DB_USER
 PASSWORD = config.DB_PASS
 PORT = config.DB_PORT
-
-# HOST = st.secrets["postgres"]['host']
-# DATABASE = st.secrets["postgres"]['dbname']
-# USER = st.secrets["postgres"]['user']
-# PASSWORD = st.secrets["postgres"]['password']
-# PORT = st.secrets["postgres"]['port']
-
 
 QUERY = """select * from assets ORDER BY date ASC;"""
 CREATE_TABLE = """
@@ -41,11 +33,8 @@ class GetDataFromDatabase:
     def __init__(self,query) -> None:
         self.query = query
         self.engine = create_engine(f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}')
-        # self.engine = create_engine(**st.secrets["postgres"])
         self.connection = psycopg2.connect(f"postgres://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}")
-        # self.connection = psycopg2.connect(**st.secrets["postgres"])
   
-
 get_data = GetDataFromDatabase(query=QUERY)
 try:
     get_data.engine.execute(text(CREATE_TABLE))
